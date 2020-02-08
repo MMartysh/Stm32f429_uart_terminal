a0fb2fcf15ff8d4dd0e931c1b281d145a9388562
skrebetsnataliia <skrebetsnatasha98@gmail.com>
2020-02-08 22:55:57 +0200

    added comments to terminal

--- terminal.h
+++ terminal.h
@@ -8,11 +8,41 @@
 #define RECEIVE_TIMEOUT 100 /*Timeout for uart_receive,100ms*/
 #define MAX_STRING_LENGTH 20/*Max lenght of data, that can be gets*/
 
+/**
+*function, which run commands
+*/
 
 void 				TERMINAL_ExecCommand(void);
+
+/**
+*function, which separates string on command name and arguments
+*identify command and run it
+*/
+
 void 				TERMINAL_Parse(void);
+
+/**
+*Initialization terminal(includes initialization all modules)
+*/
+
 void 				TERMINAL_Init(void);
+
+
+/**
+*function, which outputs similar commands, if user enter wrong command
+*/
+
 static void TERMINAL_Aliases(void);
+
+/**
+*function, which output all command
+*/
+
 static void TERMINAL_Help(void);
+
+/**
+*function, which separates string on command name, argument1, argument2
+*/
+
 static void TERMINAL_Echo(void);
 #endif
